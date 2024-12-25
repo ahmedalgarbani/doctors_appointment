25ed836f5898a7546f468c2849738dfa129ef6ae
yasser <you@yasser.com>
2024-12-25 10:46:11 +0300

    s

--- doctors_appointment/settings.py
+++ doctors_appointment/settings.py
@@ -15,9 +15,9 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-v$zu3k_g_p2dp_!2*hw!m1w2%dsl@q#&uld&7ad(wzdv#u=#*^'
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')  # يجب تعيينه باستخدام متغيرات البيئة
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -27,10 +27,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+<<<<<<< HEAD
+    'hospitals',
     'payments',
     'doctors',
     'bookings',
+=======
+    
     'hospitals',
+    'doctors',
+    'bookings',
+    'payments',
+>>>>>>> 17a6cc346d6933bc45c5346f29d0bec0ec6e5923
     'registrations',
     'notifications',
     'reports',
@@ -76,17 +84,44 @@ WSGI_APPLICATION = 'doctors_appointment.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+<<<<<<< HEAD
+        'NAME': os.environ.get('DB_NAME', 'doctor_appointment'),
+        'USER': os.environ.get('DB_USER', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
+=======
         'NAME': 'doctor_appointment',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '54321',
+>>>>>>> 17a6cc346d6933bc45c5346f29d0bec0ec6e5923
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+            'autocommit': True,
             'isolation_level': 'read committed',
-        },
+            'sql_mode': 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION',
+        }
     }
 }
+<<<<<<< HEAD
+=======
+
+DATABASE_OPTIONS = {
+    'timeout': 20,
+    'connect_timeout': 10,
+}
+
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+>>>>>>> 17a6cc346d6933bc45c5346f29d0bec0ec6e5923
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
